fc2deae365d0e4c8440467f6d4615bccc9faaa63
AbdulazizAlRabiah <38816080+AbdulazizAlRabiah@users.noreply.github.com>
2018-04-28 20:11:39 +0300

    Update MinsToYearsDays.java
    I just changed the code in my MinsToYearsDays class.

--- MinsToYearsDays.java
+++ MinsToYearsDays.java
@@ -6,8 +6,8 @@ public class MinsToYearsDays {
 		Scanner conversion = new Scanner (System.in);
 		System.out.print("Enter the number of minutes: " );
 		int time = conversion.nextInt();
-		int minsInYear = 365*24*60;
-		int years = time / minsInYear ;
+		int minutesInYear = 365*24*60;
+		int years = time / minutesInYear ;
 		int days = ( time % minsInYear) / (24*60) ;
 		System.out.println(""+time+" minutes is approximately "+years+" years and "+days+" days ");
 		
