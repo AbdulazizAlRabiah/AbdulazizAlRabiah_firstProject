790e734a0950ef95adb8f519db67b286575621a8
AbdulazizAlRabiah <38816080+AbdulazizAlRabiah@users.noreply.github.com>
2018-04-28 20:07:44 +0300

    Add files
    I uploaded two files to my repository.

---
+++ MinsToYearsDays.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MinsToYearsDays {
+
+	public static void main(String[] args) {
+		Scanner conversion = new Scanner (System.in);
+		System.out.print("Enter the number of minutes: " );
+		int time = conversion.nextInt();
+		int minsInYear = 365*24*60;
+		int years = time / minsInYear ;
+		int days = ( time % minsInYear) / (24*60) ;
+		System.out.println(""+time+" minutes is approximately "+years+" years and "+days+" days ");
+		
+
+	}
+
+}
